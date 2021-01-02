c55f281371e7436a76504ee7f99a79297635ee93
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-01 20:00:10 -0800

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Index: FC = () => {
+  return (
+    <div className='text-center py-4'>
+      <h1 className='text-4xl text-gray-700 font-bold'>HackBack</h1>
+    </div>
+  )
+}
+
+export default Index
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+    fontFamily: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
