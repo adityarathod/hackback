c074a6e4d1cce263b4a7fcb5a8609b5476371e58
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-02 14:39:30 -0800

    Make auth flow smoother, protect /home

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -18,10 +18,15 @@ const Auth: FC = () => {
   }, [isAuth])
   const login = () => firebase.auth().signInWithEmailAndPassword(username, password)
   const authForm = (
-    <main className='px-4'>
-      <div className='min-w-lg max-w-lg p-4 bg-white border border-gray-200 shadow-md rounded-md mx-auto mt-8'>
+    <main className='px-4 w-full h-screen flex items-center justify-center bg-gray-700'>
+      <div className='max-w-lg w-full p-4 bg-white border border-gray-200 shadow-md rounded-md'>
         <h1 className='text-center font-bold text-2xl text-gray-700'>Log in to HackBack</h1>
-        <div className='pt-4 pb-2 flex flex-col items-center'>
+        <form
+          onSubmit={e => {
+            login()
+            e.preventDefault()
+          }}
+          className='pt-4 pb-2 flex flex-col items-center'>
           <input
             type='email'
             placeholder='username'
@@ -37,11 +42,12 @@ const Auth: FC = () => {
             className='my-2 w-2/3 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none text-sm text-gray-800 placeholder-gray-500 border border-gray-200 rounded-md py-2 px-4'
           />
           <button
+            type='submit'
             className='mt-4 py-2 px-8 bg-blue-500 hover:bg-blue-600 text-white rounded-full'
             onClick={login}>
             Login
           </button>
-        </div>
+        </form>
       </div>
     </main>
   )
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useRouter } from 'next/router'
 import ApplicationStatus from '../components/application-status'
 import FullscreenLoader from '../components/fullscreen-loader'
 import Navbar from '../components/navbar'
@@ -6,7 +7,9 @@ import useAuth from '../hooks/useAuth'
 
 const Home: FC = () => {
   const { authInitialized, isAuth, isAdmin } = useAuth()
+  const router = useRouter()
   if (!authInitialized) return <FullscreenLoader />
+  if (!isAuth) router.replace('/auth')
   return (
     <div>
       <Navbar />
