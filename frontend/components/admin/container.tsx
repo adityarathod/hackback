fc2aec42b481357b9ad924b61d7b30d249fba783
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-18 17:27:25 -0800

    chore: minor text fixes

--- frontend/components/admin/container.tsx
+++ frontend/components/admin/container.tsx
@@ -5,7 +5,7 @@ import Nav from './nav'
 const AdminContainer: FC = () => {
   const [curTab, setCurTab] = useState(0)
   const tabList = ['Manage Applications', 'Send Decision Emails']
-  const tabs = useMemo(() => [<AppManager key='0' />, null], [])
+  const tabs = useMemo(() => [<AppManager key='0' />, <div key='1'>TBD</div>], [])
   return (
     <div>
       <Nav tabs={tabList} onTabChange={num => setCurTab(num)} curTab={curTab} />
